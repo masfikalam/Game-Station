05d66b84d9cf311901d94b965aad0e89d5e36c44
Masfikul Alam <masfikalamfrp@gmail.com>
2022-08-28 12:25:16 +0600

    project setup ready

---
+++ src/components/Header.js
@@ -0,0 +1,11 @@
+import logo from "../resources/logo1.png";
+
+const Header = () => {
+  return (
+    <header>
+      <img src={logo} alt="gamestation" />
+    </header>
+  );
+};
+
+export default Header;
